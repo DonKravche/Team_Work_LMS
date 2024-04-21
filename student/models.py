142846867ed5bc7810e26d26207bdd643357e45b
Gvantsie <g_euashvili@cu.edu.ge>
2024-04-22 00:07:46 +0400

    .

--- student/models.py
+++ student/models.py
@@ -1,44 +1,58 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-
-# Create your models here.
+from django.utils.translation import gettext as _
 
 
 class Faculty(models.Model):
-    name = models.CharField(max_length=255, verbose_name="Faculty name")
+    name = models.CharField(verbose_name=_("Faculty name"), max_length=255)
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = _('Faculty')
+        verbose_name_plural = _('Faculties')
+
 
 class Subject(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=255, verbose_name="Subject Name")
-    description = models.TextField(verbose_name="Subject Description")
-    syllabus = models.FileField(upload_to='syllabi/', verbose_name="Syllabus")
+    title = models.CharField(max_length=255, verbose_name=_("Subject Name"))
+    description = models.TextField(verbose_name=_("Subject Description"))
+    syllabus = models.FileField(upload_to='syllabus/', verbose_name=_("Syllabus"))
     faculties = models.ManyToManyField(Faculty, related_name='subjects')
     lecturers = models.ManyToManyField('Lecture', related_name='subjects')
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = _('Subject')
+        verbose_name_plural = _('Subjects')
+
 
 class Lecture(models.Model):
-    name = models.CharField(max_length=255, verbose_name="Lecture Name")
-    surname = models.CharField(max_length=255, verbose_name="Lecture Surname")
+    name = models.CharField(max_length=255, verbose_name=_("Lecture Name"))
+    surname = models.CharField(max_length=255, verbose_name=_("Lecture Surname"))
 
     def __str__(self):
         return f"{self.name} {self.surname}"
+
+    class Meta:
+        verbose_name = _('Lecture')
+        verbose_name_plural = _('Lectures')
 
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='student')
-    name = models.CharField(max_length=255, verbose_name="Student Name")
-    surname = models.CharField(max_length=255, verbose_name="Student Surname")
-    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, related_name='students', verbose_name='faculty',
+    name = models.CharField(max_length=255, verbose_name=_("Student Name"))
+    surname = models.CharField(max_length=255, verbose_name=_("Student Surname"))
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, related_name='students', verbose_name=_('Faculty'),
                                 null=True, blank=True)
-    subjects = models.ManyToManyField(Subject, verbose_name='subjects')
+    subjects = models.ManyToManyField(Subject, verbose_name=_('Subjects'))
 
     def __str__(self):
         return f"{self.name} {self.surname}"
+
+    class Meta:
+        verbose_name = _('Student')
+        verbose_name_plural = _('Students')
